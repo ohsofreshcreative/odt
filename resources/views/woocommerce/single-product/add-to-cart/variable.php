c426ebb66d7c74e96bcfb97d35f370a7135c31f2
Arek <kontakt@ohsofresh.pl>
2026-03-19 16:09:59 +0100

    Automatyczny deploy

--- resources/views/woocommerce/single-product/add-to-cart/variable.php
+++ resources/views/woocommerce/single-product/add-to-cart/variable.php
@@ -51,34 +51,10 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
         </div>
 
         <?php /* 2. CENA */ ?>
-       <!--  <?php woocommerce_template_single_price(); ?> -->
+      <?php woocommerce_template_single_price(); ?>
 
         <?php /* 3. QUANTITY + PRZYCISK */ ?>
-        <div class="woocommerce-variation-add-to-cart variations_button flex gap-4 mt-4">
-
-            <div class="quantity-wrapper flex items-center justify-center bg-white border border-gray-300 rounded-xl h-12.5 w-28">
-                <button type="button" class="quantity-button quantity-minus h-full hover:bg-gray-100 rounded-l-xl transition-all px-3">–</button>
-                <?php
-                woocommerce_quantity_input(
-                    array(
-                        'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-                        'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-                        'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(),
-                    )
-                );
-                ?>
-                <button type="button" class="quantity-button quantity-plus h-full hover:bg-gray-100 rounded-r-xl transition-all px-3">+</button>
-            </div>
-
-            <button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>">
-                <?php echo esc_html( $product->single_add_to_cart_text() ); ?>
-            </button>
-
-            <input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
-            <input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
-            <input type="hidden" name="variation_id" class="variation_id" value="0" />
-
-        </div>
+       
     <?php endif; ?>
 
     <?php do_action( 'woocommerce_after_variations_form' ); ?>
