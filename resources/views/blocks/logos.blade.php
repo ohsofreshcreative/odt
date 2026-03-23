ca1d4c8caa21d70a944fe8696b648ca4d2314518
Arek <kontakt@ohsofresh.pl>
2026-03-23 12:40:52 +0100

    Automatyczny deploy

--- resources/views/blocks/logos.blade.php
+++ resources/views/blocks/logos.blade.php
@@ -16,25 +16,15 @@ $sectionClass .= ' ' . $background;
 
 	<div class="__wrapper c-main relative">
 		<h4 data-gsap-element="header" class="w-full md:w-1/2">{{ $g_logos['title'] }}</h4>
-	</div>
 
-	@if (!empty($g_logos['gallery']))
-    <div class="swiper logos-swiper mt-20">
-        <div class="swiper-wrapper">
-            {{-- Pętla iteruje po $g_logos['gallery'], ponieważ galeria jest wewnątrz grupy --}}
-            @foreach ($g_logos['gallery'] as $image)
-            <div class="swiper-slide">
-                <img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="h-12 w-auto">
-            </div>
-            @endforeach
-            {{-- Duplikaty slajdów dla płynnej pętli --}}
-            @foreach ($g_logos['gallery'] as $image)
-            <div class="swiper-slide">
-                <img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="h-12 w-auto">
-            </div>
-            @endforeach
-        </div>
-    </div>
-    @endif
+		@if (!empty($g_logos['gallery']))
+		<div class="mt-20 flex flex-wrap items-center justify-center gap-8">
+			
+			@foreach ($g_logos['gallery'] as $image)
+			<img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="h-12 w-auto">
+			@endforeach
+		</div>
+		@endif
+	</div>
 
 </section>
