3e6cd900ea97d67f655c083a95471f3a0ee7a6f9
Arek <kontakt@ohsofresh.pl>
2026-03-26 12:37:06 +0100

    Automatyczny deploy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,14 @@
 <html @php(language_attributes())>
 
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M5TV295L');</script>
+<!-- End Google Tag Manager --> 
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	@php(do_action('get_header'))
@@ -17,6 +25,10 @@
 </head>
 
 <body @php(body_class())>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5TV295L"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 	@php(wp_body_open())
 
 	<div id="app">
