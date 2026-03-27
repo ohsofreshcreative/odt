7cffe6d9edf6d7f44e2db68e24cebd7b95644840
Arek <kontakt@ohsofresh.pl>
2026-03-27 13:58:55 +0100

    Automatyczny deploy

--- resources/views/woocommerce/checkout/cart-errors.php
+++ resources/views/woocommerce/checkout/cart-errors.php
@@ -18,8 +18,11 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<p><?php esc_html_e( 'There are some issues with the items in your cart. Please go back to the cart page and resolve these issues before checking out.', 'woocommerce' ); ?></p>
+<div class="c-main">
+<p><?php esc_html_e( 'tttThere are some issues with the items in your cart. Please go back to the cart page and resolve these issues before checking out.', 'woocommerce' ); ?></p>
 
 <?php do_action( 'woocommerce_cart_has_errors' ); ?>
 
 <p><a class="button wc-backward" href="<?php echo esc_url( wc_get_cart_url() ); ?>"><?php esc_html_e( 'Return to cart', 'woocommerce' ); ?></a></p>
+
+</div>
