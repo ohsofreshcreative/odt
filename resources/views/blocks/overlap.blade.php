5ea4f0790b5afc78a9b840d2dd28f81fc344aa30
Arek <kontakt@ohsofresh.pl>
2026-04-03 13:29:28 +0200

    Automatyczny deploy

--- resources/views/blocks/overlap.blade.php
+++ resources/views/blocks/overlap.blade.php
@@ -44,5 +44,5 @@
 		</div>
 	</div>
 
-	<img data-gsap-element="bg" class="__bg absolute w-[400px] -left-60 top-32 pointer-events-none" src="/wp-content/uploads/2025/12/sign_small.svg" />
+	<img data-gsap-element="bg" class="__bg absolute w-[400px] -left-60 top-32 pointer-events-none" src="/wp-content/uploads/2026/01/quality-plant.svg" />
 </section>
